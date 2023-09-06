41716dae47e584ad45dd788658ec1395f22ff994
Nham <nguyenlamhai89@gmail.com>
2023-09-06 14:55:54 +0700

    Sep 06 - 14:55

---
+++ Health/components/favCard_Compact.swift
@@ -0,0 +1,20 @@
+//
+//  favCard_Compact.swift
+//  Health
+//
+//  Created by nham on 06/09/2023.
+//
+
+import SwiftUI
+
+struct favCard_Compact: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct favCard_Compact_Previews: PreviewProvider {
+    static var previews: some View {
+        favCard_Compact()
+    }
+}
---
+++ Health/components/favCard_Mini.swift
@@ -0,0 +1,20 @@
+//
+//  favCard_Mini.swift
+//  Health
+//
+//  Created by nham on 06/09/2023.
+//
+
+import SwiftUI
+
+struct favCard_Mini: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct favCard_Mini_Previews: PreviewProvider {
+    static var previews: some View {
+        favCard_Mini()
+    }
+}
