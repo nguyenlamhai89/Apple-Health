722542b9f15fb7a053c6103e466f16a2dc3743a2
Nham <nguyenlamhai89@gmail.com>
2023-09-06 15:33:59 +0700

    Sep 06 - 15:33

---
+++ Health/components/favCard_Medium.swift
@@ -0,0 +1,20 @@
+//
+//  favCard_Medium.swift
+//  Health
+//
+//  Created by nham on 06/09/2023.
+//
+
+import SwiftUI
+
+struct favCard_Medium: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct favCard_Medium_Previews: PreviewProvider {
+    static var previews: some View {
+        favCard_Medium()
+    }
+}
