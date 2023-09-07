af74cdf340124923de33b1d067925856c2f97e22
Nham <nguyenlamhai89@gmail.com>
2023-09-07 16:17:12 +0700

    Sep 07 - 16:18

--- Health/components/appCard.swift
+++ Health/components/appCard.swift
@@ -8,13 +8,38 @@
 import SwiftUI
 
 struct appCard: View {
+    var imgApp: String
+    var nameApp: String
+    var descApp: String
+    @Binding var isShown: Bool
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-struct appCard_Previews: PreviewProvider {
-    static var previews: some View {
-        appCard()
+        HStack (spacing: 12) {
+            Image(systemName: imgApp)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 32)
+                .foregroundColor(Color.red)
+                .symbolRenderingMode(.hierarchical)
+            HStack {
+                VStack (alignment: .leading, spacing: 4) {
+                    Text(nameApp)
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                    Text(descApp)
+                        .font(.caption2)
+                        .foregroundColor(Color.gray)
+                }
+                Spacer()
+                Button {
+                    print("Clicked")
+                    isShown.toggle()
+                } label: {
+                    Image(systemName: "info.circle")
+                        .foregroundColor(Color.gray)
+                }
+            }
+        }.padding(.vertical, 4)
+        
+        
     }
 }
--- Health/components/articleCard.swift
+++ Health/components/articleCard.swift
@@ -8,13 +8,23 @@
 import SwiftUI
 
 struct articleCard: View {
+    var imgName: String
+    var cardName: String
+    var cardContent: String
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-struct articleCard_Previews: PreviewProvider {
-    static var previews: some View {
-        articleCard()
+        VStack (alignment: .leading, spacing: 12) {
+            Image(imgName)
+                .resizable()
+                .scaledToFit()
+                .cornerRadius(8)
+            VStack (alignment: .leading, spacing: 8) {
+                Text(cardName)
+                    .font(.headline)
+                    .fontWeight(.bold)
+                Text(cardContent)
+                    .font(.subheadline)
+                    .foregroundColor(Color.gray)
+            }
+        }
     }
 }
